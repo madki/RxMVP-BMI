11e32c2e7c12403211db4a6e89cf0ba4c756ac68
themadhukiran <pmadhukiran96@gmail.com>
2016-09-03 04:39:21 +0530

    add mockito and one unit test

---
+++ app/src/test/java/xyz/madki/rxmvp/bmi/MainPresenterTests.java
@@ -0,0 +1,35 @@
+package xyz.madki.rxmvp.bmi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import rx.subjects.PublishSubject;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainPresenterTests {
+
+  @Mock MainPresenter.IView view;
+
+  @Test
+  public void bmiStringTest_validInputs() throws Exception {
+    PublishSubject<String> mockHeight$ = PublishSubject.create();
+    PublishSubject<String> mockWeight$ = PublishSubject.create();
+
+    MainPresenter presenter = new MainPresenter();
+    when(view.height$()).thenReturn(mockHeight$.asObservable());
+    when(view.weight$()).thenReturn(mockWeight$.asObservable());
+
+    presenter.takeView(view);
+
+    mockHeight$.onNext("400");
+    mockWeight$.onNext("16");
+
+    verify(view, times(1)).setBMI("BMI: 1.00");
+  }
+}
